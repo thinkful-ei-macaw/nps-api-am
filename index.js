78012ee9b75f86102abeee3098f003a4475eeecf
Malcolm Kiano <malcolmkiano@gmail.com>
2020-02-20 17:56:51 -0500

    Added documentation to functions, tidied HTML

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
+/* global $ */
 
+/**
+ * starts listeners
+ */
 function init() {
   parksFormSubmit();
 }
 
+/**
+ * listeners for submit of parks form (#national-parks)
+ * and sends API request
+ */
 function parksFormSubmit() {
   $('#national-parks').submit( function(e) {
     e.preventDefault();
@@ -13,37 +21,51 @@ function parksFormSubmit() {
   });
 }
 
+/**
+ * 
+ * @param {string} park the query entered into 'park name'
+ * @param {string} state the value entered into 'state'
+ * @param {number} maxResults the value entered into 'maxResults'
+ */
 function sendRequest(park, state, maxResults ) {
-  const apiKey = "1Hge8qs8XNqiYFIF1HySn4MhboakRjzWjVKUV3UF";
+  const apiKey = '1Hge8qs8XNqiYFIF1HySn4MhboakRjzWjVKUV3UF';
   fetch(`https://developer.nps.gov/api/v1/parks?q=${park}&stateCode=${state}&limit=${maxResults}&api_key=${apiKey}`)
     .then(response => response.json())
     .then(responseJson => displayParks(responseJson))
     .catch(() => alert('Something went wrong. Try again later.'));
 }
 
-function generateParks(response) {
+/**
+ * Renders a JSON response object to the DOM
+ * @param {object} response JSON response object
+ */
+function displayParks(response) {
+  let html = generateParkTemplates(response);
+  $('#parks-display').html(html);
+}
 
+/**
+ * Generates HTML from a JSON response object
+ * @param {object} response JSON response object
+ */
+function generateParkTemplates(response) {
   let html = '';
   const parks = response.data;
   
-
   parks.forEach(item => {
-    html += 
-            `<article>
-                <h2><a href="${item.url}">${item.fullName}</a></h2>
-                <p>${item.description}</p>
-            </article>`;
+    html += `
+      <article>
+        <h2><a href="${item.url}" target="_blank">${item.fullName}</a></h2>
+        <p>${item.description}</p>
+      </article>
+    `;
   });
 
+  // return html or no results if none available
   return parks.length > 0 ? html : '<p>No results found</p>';
 }
 
-function displayParks(response) {
 
-  let html = generateParks(response);
-  $('#parks-display').html(html);
-  
-}
 
 $(init);
 
